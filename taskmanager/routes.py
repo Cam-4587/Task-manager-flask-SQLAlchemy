0604f769cf0f2da629b3172cdda709414b8d7344
Cam-4587 Conroy <cameronconroy@live.com>
2025-02-25 19:12:42 +0000

    Finish Viewing Categories section

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -8,7 +8,8 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
@@ -16,5 +17,5 @@ def add_category():
         category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
-        return redirect(url_for("Categories"))
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
