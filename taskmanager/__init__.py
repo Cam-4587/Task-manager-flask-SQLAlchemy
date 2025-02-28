3bd24a99681ed4340885988415e9523052f25a0e
Cam-4587 Conroy <cameronconroy@live.com>
2025-02-28 14:06:11 +0000

    Add functionality to Delete Categories

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -10,7 +10,7 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 db = SQLAlchemy(app)
 
 
