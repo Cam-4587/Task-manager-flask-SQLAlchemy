81963d1b99f8df6b4a7930d6a4b904fced797d6f
Cam-4587 Conroy <cameronconroy@live.com>
2025-03-05 17:33:39 +0000

    ALter __init__.py file to remove if statement

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,12 +7,11 @@ from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env # noqa
     
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+db = SQLAlchemy(app)
 
 
 from taskmanager import routes # noqa
